d800f7b5aa66e8777895b4050b97bf7ddf5ece24
polyfooly <vlaso.kirill2011@gmail.com>
2020-01-04 22:56:12 +0100

    nix script refactored

--- default.nix
+++ default.nix
@@ -13,30 +13,43 @@
 
   withHoogle = false;
 
-  overrides = self: super: rec {
+  overrides = self: super: (builtins.listToAttrs 
+  ( (map (x: { name = x; value = pkgs.haskell.lib.dontCheck super.${x}; }) 
+    [ "Glob"
+      "wai-extra"
+      "wai-app-static"
+      "silently"
+      "hpack"
+      "mockery" 
+      "unliftio" 
+      "yaml"
+      "conduit"
+      "brittany" 
+      "reflex-dom-core"
+      "eventstore"
+    ]) ++
+    (map (x: { name = x; value = pkgs.haskell.lib.dontCheck
+      ( pkgs.haskell.lib.overrideCabal super.${x} (drv: 
+        { testHaskellDepends = []; })
+      ); })
+    [ "servant"
+      "servant-server"
+      "http-date"
+      "iproute"
+      "unix-time"
+      "http2"
+      "bsb-http-chunked"
+    ]) ++
+    (map (x: { name = x; value = pkgs.haskell.lib.dontHaddock super.${x}; })
+    [ "frontend"
+      "common"
+      "node"
+      "frontendhost"
+      "posts"
+      "ib2-lib"
+    ])
+  )) // {
     servant-reflex = self.callPackage ./packages/servant-reflex.nix {};
-
-    servant = pkgs.haskell.lib.dontCheck ( pkgs.haskell.lib.overrideCabal super.servant (drv: {
-      testHaskellDepends = []; # testdoc broken dependency
-    }));
-
-    # all these test would fail for some reason during GHCJS build (maybe its bad?)
-    # TODO: refactor it
-    Glob = pkgs.haskell.lib.dontCheck super.Glob;
-    silently = pkgs.haskell.lib.dontCheck super.silently;
-    hpack = pkgs.haskell.lib.dontCheck super.hpack;
-    mockery = pkgs.haskell.lib.dontCheck super.mockery;
-    unliftio = pkgs.haskell.lib.dontCheck super.unliftio;
-    yaml = pkgs.haskell.lib.dontCheck super.yaml;
-    conduit = pkgs.haskell.lib.dontCheck super.conduit;
-    brittany = pkgs.haskell.lib.dontCheck super.brittany;
-    reflex-dom-core = pkgs.haskell.lib.dontCheck super.reflex-dom-core;
-    eventstore = pkgs.haskell.lib.dontCheck super.eventstore;
-
-    # TODO: maybe optional dontHaddock for jsaddle, servant (due to too long first build)
-    backend = pkgs.haskell.lib.dontHaddock super.backend;
-    common = pkgs.haskell.lib.dontHaddock (pkgs.haskell.lib.overrideCabal super.common (drv: { libraryToolDepends = []; }));
-    frontend = pkgs.haskell.lib.dontHaddock (pkgs.haskell.lib.overrideCabal super.frontend (drv: { libraryToolDepends = []; }));
   };
 
   shells = {
