453b2533479cf304ce76037a364cc08a76195197
polyfooly <vlaso.kirill2011@gmail.com>
2020-02-04 20:15:14 +0200

    fixed incorrect index.html fallback behavior

--- backend/frontendhost/src/lib/FrontendHost.hs
+++ backend/frontendhost/src/lib/FrontendHost.hs
@@ -19,12 +19,19 @@ server :: String -> Server FrontendHostAPI
 server dir = serveDirectoryWith serveSettings
     where
         serveSettings = defSettings { ssLookupFile = lookupFile }
+        indexPiece = [unsafeToPiece "index.html"]
         defSettings = (defaultFileServerSettings dir)
         lookupFile p = do
             f <- ssLookupFile defSettings p
+            let defAction = ssLookupFile defSettings indexPiece
             case f of
                 LRFile _ -> return f
-                _ -> ssLookupFile defSettings [unsafeToPiece "index.html"]
+                -- a workaround to fix an issue with relative links in index.html
+                -- when falling back to it from nonexistent route
+                LRNotFound -> if length p > 1
+                    then lookupFile $ tail p
+                    else defAction
+                _ -> defAction
 
 app :: String -> Application
 app dir = serve frontendHostAPI $ server dir
